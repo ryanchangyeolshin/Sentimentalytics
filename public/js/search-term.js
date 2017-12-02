b2c43a354b317a25626dd003817938613522e605
Ryan Shin <changyeolshin@gmail.com>
2017-12-02 02:17:14 -0800

    Created functionality to render the search term list item. #13

--- public/js/search-term.js
+++ public/js/search-term.js
@@ -4,7 +4,10 @@ function renderSearchTerm(data, id) {
   const $sentiment = document.createElement('a')
   const $strong = document.createElement('strong')
   $sentiment.setAttribute('class', 'sentiment list-group-item list-group-item-action my-lg-3 white-text animated bounceInUp')
-  $sentiment.setAttribute('id', id)
+  $sentiment.setAttribute('id', `sentiment-${id}`)
+  $sentiment.setAttribute('data-id', id)
+  $sentiment.setAttribute('sentiment', data['sentiment'])
+  $sentiment.setAttribute('confidence', data['confidence'])
   $strong.textContent = data['term']
   $sentiment.appendChild($strong)
   return $sentiment
